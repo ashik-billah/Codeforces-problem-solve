541d5bc1b69bf10eac7a7c58e1c8a019001ea4d0
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-11-12 15:03:46 +0600

    A. Satisfying Constraints

---
+++ A. Satisfying Constraints.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+        int n,a,x;
+        cin>>n;
+        for(int i=1;i<=n;i++)
+        {
+            cin>>a>>x;
+
+        }
+        cout<<a<< " "<<x<<endl;
+    }
+    return 0;
+}
