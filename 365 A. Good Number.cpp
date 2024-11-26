afc98a85cf16913f1a63eaa331bcea9e6c2c687a
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-11-26 18:00:29 +0600

    A. Good Number

---
+++ 365 A. Good Number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+int arr[10];
+using namespace std;
+int main() {
+	int n,k;
+	cin>>n>>k;
+	vector<string> vect;
+	for(int i=0;i<n;i++){
+		string s;
+		cin>>s;
+
+		vect.push_back(s);
+	}
+	int answer=0;
+  
+	for(int i=0;i<n;i++){
+		int val=0;
+		for(int j=0;j<=k;j++){
+		if(	vect[i].find(to_string(j)) !=-1)
+			++val;
+			
+		}
+		if(val==k+1)
+ 		++answer;
+		
+	}
+ 
+cout<<answer<<endl;
+ 
+	return 0;
+}
