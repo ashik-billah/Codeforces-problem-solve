58b1c0f9d91339773428da4691764ea30b69fffc
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-11-28 18:05:56 +0600

    B. Garland

---
+++ 408B. Garland.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int arr[26];
+int arr2[26];
+int main() {
+
+string s,s2;
+int sum=0;
+cin>>s>>s2;
+int len1=s.length();
+int len2=s2.length();
+for(int i=0;i<len1;i++){
+	++arr[s[i]-'a'];
+}
+for(int i=0;i<len2;i++){
+	++arr2[s2[i]-'a'];
+}
+for(int i=0;i<26;i++){
+	if(arr2[i])
+	{
+		if(arr[i])
+	sum+=min(arr[i],arr2[i]);
+		else{
+			sum=0;
+			break;
+		}
+	}
+}
+
+if(sum){
+
+	cout<<sum;
+}
+else{
+	cout<<"-1";
+}
+	return 0;
+}
