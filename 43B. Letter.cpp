7c0caf175e8eb5ad8a50d9262f4917009abdf124
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-11-30 09:29:29 +0600

    B. Letter

---
+++ 43B. Letter.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<string>
+#include<map>
+using namespace std;
+
+int main() {
+	string s,s2;
+getline(cin,s);
+getline(cin,s2);
+map<char,int> ma;
+int len=s.length();
+for(int i=0;i<len;i++){
+	++ma[s[i]];
+}
+int counter=0;
+int len2=s2.length();
+int chars=0;
+for(int i=0;i<len2;i++){
+	if(s2[i]!=' '){
+		++chars;
+		if(ma[s2[i]]>0){
+			++counter;
+			--ma[s2[i]];
+		}
+		
+	}
+}
+
+	
+if(chars==counter){
+	cout<<"YES"<<endl;
+}
+
+else {
+	cout<<"NO"<<endl;
+	
+}
+return 0;
+}
