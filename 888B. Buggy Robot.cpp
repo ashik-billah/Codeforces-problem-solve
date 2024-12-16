29a01694834c8e1d1997eeba55d43d9b32a370d3
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-12-16 18:12:19 +0600

    B. Buggy Robot

---
+++ 888B. Buggy Robot.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+int n;
+cin>>n;
+string s;
+int x=0,y=0;
+cin>>s;
+for(int i=0;i<n;i++){
+if(s[i]=='U'){
+++y;
+}
+else if(s[i]=='D'){
+	--y;
+}
+else if(s[i]=='L'){
+	--x;
+}
+else if(s[i]=='R'){
+++x;
+}
+}
+int val1=abs(x)+abs(y);
+cout<<n-val1;
+	return 0;
+}
