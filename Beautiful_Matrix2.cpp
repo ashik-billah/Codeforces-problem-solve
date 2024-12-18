503cc2d9ef473ece8604408f13690643586d90c5
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-12-19 05:24:50 +0600

    Beautiful_Matrix2

---
+++ Beautiful_Matrix2.cpp
@@ -0,0 +1,25 @@
+#include<cmath>
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[5][5];
+	int goal_x=2;
+	int goal_y=2;
+	int x;
+	int y;
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++)
+		{
+			cin>>arr[i][j];
+			if((arr[i][j])==1){
+				x=i;
+				y=j;
+
+			}
+		}
+	}
+	cout<<abs(goal_x-x)+abs(goal_y-y);
+
+	return 0;
+}
