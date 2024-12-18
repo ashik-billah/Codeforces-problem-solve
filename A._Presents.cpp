836ba9e6d90bc01e57203b827700d6237e4d5a90
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-12-18 15:18:44 +0600

    A._Presents

---
+++ A._Presents.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numOFperson;
+    int arr1[101];
+    int arr2[101];
+    cin>>numOFperson;
+    for(int i=0;i<numOFperson;i++){
+
+    cin>>arr1[i];
+
+
+    }
+for(int j=0;j<numOFperson;j++){
+    arr2[arr1[j]]=j+1;
+
+    }
+    for(int x=1;x<=numOFperson;x++){
+        cout<<arr2[x]<<" ";
+    }
+
+}
