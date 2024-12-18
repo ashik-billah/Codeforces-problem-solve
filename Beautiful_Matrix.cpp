06a972b65fb1f99853261a1e8ccd371c1706f9ba
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-12-19 05:24:15 +0600

    Beautiful_Matrix

---
+++ Beautiful_Matrix.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[5][5];
+	int ineed;
+	int jneed;
+	int tj=2;
+	int ti=2;
+
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++)
+		{
+			int x;
+			cin>>x;
+			if(x==1){
+				ineed=i;
+				jneed=j;
+			}
+			arr[i][j]=x;
+		}
+
+
+}
+	int v1=abs(ineed-ti);
+			int v2=abs(jneed-tj);
+			int result=0;
+			result=v1+v2;
+			cout<<result;
+
+	return 0;
+}
