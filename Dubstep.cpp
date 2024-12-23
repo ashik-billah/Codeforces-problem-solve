28fed5435d17e905846cf11380d3e357dcaff121
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2024-12-23 23:36:55 +0600

    Dubstep

---
+++ Dubstep.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+string s1,s2;
+cin>>s1;
+while(s1.find("WUB")!=-1){
+	int x=s1.find("WUB");
+	if(x==0){
+		s1.erase(0,3);
+	}
+	else{
+	s1.replace(x,1," ");
+	s1.erase(x+1,2);
+	}
+}
+cout<<s1;
