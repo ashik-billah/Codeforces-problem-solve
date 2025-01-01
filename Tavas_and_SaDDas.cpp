afaf6af77dca30a522d1b25b6ac2a1b6d52e959c
Md Ashik Billah <ashik.210101@s.pust.ac.bd>
2025-01-01 21:48:44 +0600

    Tavas_and_SaDDas

---
+++ Tavas_and_SaDDas.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<algorithm>
+#include <vector>
+
+using namespace std;
+vector <int>v;
+
+void x(int n){
+	if(n>100000000){
+		return ;
+	}
+	v.push_back(n*10+4);
+	v.push_back(n*10+7);
+	int w=0;
+	w=v.size();
+	x(v[w-1]);
+	x(v[w-2]);
+}
+int main() {
+int n;
+cin>>n;
+x(0);
+sort(v.begin(),v.end());
+int q=v.size();
+for(int i=0;i<q;i++){
+	if(v[i]==n)
+		cout<<i+1;
+}
+
+	return 0;
+}
